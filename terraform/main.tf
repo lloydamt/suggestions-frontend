515ca83197f7aef48d2cc33e0cf084cb0b0747d6
Adawari Thomas <adawari.thomas@bjss.com>
2024-09-12 23:16:54 +0100

    create actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -118,23 +118,3 @@ resource "aws_s3_bucket_website_configuration" "website-config" {
 output "website_url" {
     value = aws_s3_bucket.frontend-bucket.website_endpoint
 }
-
-
-# resource "aws_s3_bucket_website_configuration" "website-config" {
-#   bucket = data.aws_s3_bucket.selected-bucket.bucket
-#   index_document {
-#     suffix = "index.html"
-#   }
-#   error_document {
-#     key = "404.jpeg"
-#   }
-#   # IF you want to use the routing rule
-#   routing_rule {
-#     condition {
-#       key_prefix_equals = "/abc"
-#     }
-#     redirect {
-#       replace_key_prefix_with = "comming-soon.jpeg"
-#     }
-#   }
-# }
